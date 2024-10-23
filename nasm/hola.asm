65913bd09f3d0b98d528386604b4131b8f1e7820
AngelicaReyesB <angelicam.reyesb@uqvirtual.edu.co>
2024-10-23 10:55:42 -0500

    cambios

--- nasm/hola.asm
+++ nasm/hola.asm
@@ -1,2 +1,4 @@
-hola
-juanita = mi mazorca
+section .data
+    msg_intro db "Bienvenido a la calculadora en ensamblador (RPN)", 0xA, 0xD
+    prompt_num db "Ingrese dos numeros en RPN separados por espacio: ", 0xA, 0xD
+    prompt_
