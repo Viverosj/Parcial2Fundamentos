db2fa1e7aadf54b6528079323f37d61d1604dadf
AngelicaReyesB <angelicam.reyesb@uqvirtual.edu.co>
2024-10-24 10:20:13 -0500

    1

--- nasm/hola.asm
+++ nasm/hola.asm
@@ -1,15 +1,19 @@
 section .data
     msg_intro db "Bienvenido a la calculadora en ensamblador (RPN)", 0xA, 0xD               
-    prompt_num db "Ingrese dos numeros en RPN separados por espacio: ", 0xA, 0xD
+    prompt_num1 db "Ingrese el primer número: ", 0xA, 0xD
+    prompt_num2 db "Ingrese el segundo número: ", 0xA, 0xD
     prompt_op db "Ingrese la operación a realizar (+, -, *, /, %): ", 0xA, 0xD
-    error_zero db "Error: DIvisión por cero", 0xA, 0xD
-    msg_res db "Resultados: ", 0xA, 0xD
+    error_zero db "Error: División por cero", 0xA, 0xD
+    error_op db "Error: División por cero", 0xA, 0xD
+    msg_res db "Resultado: ", 0xA, 0xD
     exit_msg db "Esriba 'exit' para salir o cualquier tecla para continuar.", 0xA, 0xD
 
 section .bss 
+    num1 resb 10
+    num2 resb 10
+    result resb 10
     buffer resb 30                                          ;Espacio para leer la entrada del ususario
-    num_stack resd 10                                       ;Espacio para una pila de números (máximo 10 números)
-    stack_ptr resb 1                                        ;Puntero de la pila
+    operation resb 2
 
 section .text 
     global _start                                           ;Se hace la etiqueta _start global para que sea el punto de entrada
@@ -18,50 +22,54 @@ _start:                                                     ; Muestra la bienven
     mov eax, 4                                              ;Código del sistema (sys_write, para escribir)
     mov ebx, 1                                              ;File descriptor (1 = salida estandar de la pantalla)
     mv ecx, msg_intro                                       ;Dirección de la cadena de texto del mensaje de entrada
-    mov edx, 47                                             ;Longitud de la cadena (37 caracteres)
+    mov edx, 36                                             ;Longitud de la cadena (37 caracteres)
     int 0x80                                                ;Llamada a la interrupción del sistema
 
 loop:                                                       ; Solicitar los números
     mov eax, 4
     mov ebx, 1
-    mov ecx, prompt_num                                     ;Dirección de la cadena de texto del mesnaje para ingresar dos números
-    mov edx, 49
+    mov ecx, prompt_num1                                     ;Dirección de la cadena de texto del mesnaje para ingresar dos números
+    mov edx, 26
     int 0x80
 
     ; Leer entrada del usuario
     mov eax, 3
     mov ebx, 0
-    mov ecx, buffer
-    mov edx, 30
+    mov ecx, num1
+    mov edx, 10
+    int 0x80
+    call parse_number1                                      ;Convertir la entrada en número
+
+    ; Solicitar el segundo número 
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, prompt_num2
+    mov edx, 26
     int 0x80
 
-    ; Inicializar el puntero de la pila
-    mov byte [stack_ptr], 0
+    ;Leer el segundo número
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, num2mov edx, 10
+    int 0x80
+    call parse_number2                                      ;Convertir la entrada en número
 
-    ; Se parsea y procesa la entrada en RPN
-    mov esi, buffer                                         ;Apuntar a la entrada
-    
-parse_input:
-    mov al, [esi]                                           ;Leer caracyer actual
-    cmp al, 0x20                                            ;Comprueba si es un espacio
-    je next_char                                            ;Ignora los espacios
+    ; SOlicitar la operación
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, prompt_op
+    mov edx, 35
+    int 0x80
 
+    ; Leer la operación
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, operation
+    mov edx, 2
+    int 0x80
 
-    cmp al, 0x0A                                            ;Comprueba si es un salto de línea (Fin de la emtrada)
-    je mostrar_res
-
-    ; verifica si es un operador o un número
-    cmp al, '0'
-    jl check_operator                                       ;Si es menor que 0 debe ser un operador
-    cmp al, '9'                                             
-    jg check_operator                                       ;Si es mayor que 9 debe ser un operador
-
-    ; Es un número, convertir y pushear a la pila
-    sub al, '0'                                             ;Convertir de ASCII a número
-    call push_stack
-    jmp next_char
-
-check_operator:
+    ; Realizar la operación
+    mov al, [operation]
     cmp al, '+'
     je suma
     cmp al, '-'
